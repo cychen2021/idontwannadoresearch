6b741e2d29cf196352638bec7a65b5cc7f5662c3
Chuyang Chen <chuyangchen2018@outlook.com>
2024-09-12 22:35:27 -0400

    Append diagnostic to the watch message

--- idontwannadoresearch/watch.py
+++ idontwannadoresearch/watch.py
@@ -7,6 +7,7 @@ def watch(logger: Mailogger, message: str | None = None):
             try:
                 return func(*args, **kwargs)
             except Exception as e:
-                logger.log('You are doomed!' if message is None else message, str(e))
+                logger.log('You are doomed!' if message is None else message, 
+                           f'Exception: \n{e} \n------\nDiagnostic: \n{diagnose()}')
         return wrapper
     return __watch

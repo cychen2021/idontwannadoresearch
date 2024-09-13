629ebb7129f1f7623603afe42fba1418df73dd77
Chuyang Chen <chuyangchen2018@outlook.com>
2024-09-12 23:04:42 -0400

    Add a report_ok function

--- idontwannadoresearch/watch.py
+++ idontwannadoresearch/watch.py
@@ -1,13 +1,19 @@
 from .mailog import Mailogger
 from .diagnostic import Diagnostic, diagnose
 
-def watch(logger: Mailogger, message: str | None = None):
+def watch(logger: Mailogger, 
+          error_message: str | None = None,
+          *, report_ok: bool = False, ok_message: str | None = None):
     def __watch(func):
         def wrapper(*args, **kwargs):
             try:
-                return func(*args, **kwargs)
+                result = func(*args, **kwargs)
             except Exception as e:
-                logger.log('You are doomed!' if message is None else message, 
+                logger.log('You are doomed!' if error_message is None else error_message, 
                            f'Exception: \n{e} \n------\nDiagnostic: \n{diagnose()}')
+            else:
+                if report_ok:
+                    logger.log('Successfully completed' if ok_message is None else ok_message)
+                return result
         return wrapper
     return __watch

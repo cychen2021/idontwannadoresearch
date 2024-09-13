2afdee50fe4945cae3c5fe019e48fc466dc13550
Chuyang Chen <chuyangchen2018@outlook.com>
2024-09-13 14:42:42 -0400

    Fix a bug

--- idontwannadoresearch/mapreduce/accumulate.py
+++ idontwannadoresearch/mapreduce/accumulate.py
@@ -1,8 +1,8 @@
 from typing import Any, Callable
 from .mapping import Mapping
 
-class Accumulate[T, R, T1]:
-    def __init__(self, acc: Callable[[list[T]], R], mapping: Mapping[T1, T] | None = None) -> None:
+class Accumulate[T, R]:
+    def __init__(self, acc: Callable[[list[T]], R], mapping: Mapping[Any, T] | None = None) -> None:
         self.acc = acc
         self.mapping = mapping
         
@@ -13,5 +13,5 @@ class Accumulate[T, R, T1]:
         assert self.mapping is not None, "Mapping is not set"
         return self.acc(self.mapping())
 
-def accumulate[T, R](acc: Callable[[list[T]], R]) -> Accumulate[T, R, object]:
+def accumulate[T, R](acc: Callable[[list[T]], R]) -> Accumulate[T, R]:
     return Accumulate(acc, None)
--- idontwannadoresearch/mapreduce/mapping.py
+++ idontwannadoresearch/mapreduce/mapping.py
@@ -1,36 +1,34 @@
-from typing import Callable
+from typing import Callable, Sequence
 from .segment import Segment
 from .accumulate import Accumulate
 import concurrent.futures
 
 class Mapping[T, R]:
-    def __init__(self, mapper: Callable[[T], R], segment: Segment[T] | None) -> None:
+    def __init__(self, mapper: Callable[[Sequence[T]], R], segment: Segment[T] | None) -> None:
         self.mapper = mapper
         self.segment = segment
 
-    def map(self, data: T) -> R:
+    def map(self, data: Sequence[T]) -> R:
         return self.mapper(data)
     
     def __call__(self) -> list[R]:
         assert self.segment is not None, "Segment is not set"
         segments = self.segment()
         
-        def wrapper(data: list[T]) -> list[R]:
-            return list(map(self.map, data))
-        
         futures = []
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            future = executor.map(wrapper, segments)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=len(segments)) as executor:
+            future = executor.map(self.map, segments)
             futures.append(future)
+        assert len(futures) == len(segments), "Length of futures and segments are not equal"
         result = []
         for future in concurrent.futures.as_completed(futures):
             result.extend(future.result())
 
         return result
 
-    def __rshift__[T1](self, accumulate: Accumulate[R, T1, T]) -> R:
+    def __rshift__[R1](self, accumulate: Accumulate[R, R1]) -> R:
         accumulate.mapping = self
         return accumulate()
 
-def mapping[T, R](mapper: Callable[[T], R]) -> Mapping[T, R]:
+def mapping[T, R](mapper: Callable[[Sequence[T]], R]) -> Mapping[T, R]:
     return Mapping(mapper, None)

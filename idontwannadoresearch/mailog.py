580b50d3c8ffeec20a6f5d59adb7e084d3c86049
Chuyang Chen <chuyangchen2018@outlook.com>
2025-06-04 11:28:33 -0400

    Add configuration loading for Mailogger from TOML file

--- idontwannadoresearch/mailog.py
+++ idontwannadoresearch/mailog.py
@@ -4,21 +4,45 @@ from datetime import datetime
 import os
 import sys
 import logging
+import tomllib
 
 class Mailogger:
+    @staticmethod
+    def load_from_config(file: str, chained_logger: logging.Logger | None = None) -> 'Mailogger':
+        with open(file, 'rb') as f:
+            config = tomllib.load(f)
+        logging_config = config.get('logging', {})
+        disabled = not logging_config.get('enable_email', False)
+        email_send = logging_config.get('email_send', None)
+        email_receive = logging_config.get('email_receive', None)
+        email_smtp_server = logging_config.get('email_smtp_server', 'smtp.gmail.com')
+        email_smtp_port = logging_config.get('email_smtp_port', 587)
+        email_smtp_password = logging_config.get('email_smtp_password', None)
+        return Mailogger(
+            identifier=config.get('identifier', 'default'),
+            smtp_server=email_smtp_server,
+            sender_email=email_send,
+            smtp_port=email_smtp_port,
+            receiver_email=email_receive,
+            chained_logger=chained_logger,
+            disabled=disabled,
+            password=email_smtp_password
+        )
+
     def __init__(self, identifier: str, 
                  smtp_server: str, sender_email: str, smtp_port: int,
-                 receiver_email: str, chained_logger: logging.Logger | None = None) -> None:
-        if 'MAILOG_DISABLE' in os.environ and os.environ['MAILOG_DISABLE'] == '1':
+                 receiver_email: str, chained_logger: logging.Logger | None = None,
+                 disabled: bool | None = None, password: str | None = None) -> None:
+        if (disabled is not None and disabled) or 'MAILOG_DISABLE' in os.environ and os.environ['MAILOG_DISABLE'] == '1':
             self.disabled = True
         else:
             self.disabled = False
-        if not self.disabled and 'MAILOG_PASSWORD' not in os.environ:
+        if not self.disabled and password = None and 'MAILOG_PASSWORD' not in os.environ:
             print("Please set the MAILOG_PASSWORD environment variable")
             sys.exit(1)
         self.sender_email = sender_email
         if not self.disabled:
-            self.sender_password = os.environ['MAILOG_PASSWORD']
+            self.sender_password = os.environ['MAILOG_PASSWORD'] if password is None else password
         self.smtp_server = smtp_server
         self.smtp_port = smtp_port
         self.identifier = identifier
@@ -32,7 +56,7 @@ class Mailogger:
         timestamp = datetime.now().strftime(r'%Y-%m-%d %H:%M:%S')
         header = f'[mailog:{self.identifier}] {subject}'
         body = body + f'\n---timestamp: {timestamp}---\n'
-        
+    
         msg['Subject'] = header
         msg.set_content(body)
 
